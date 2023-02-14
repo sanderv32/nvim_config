65ee71d2e0e5ba14437617f3682e75295a3c939f
Alexander Verhaar <averhaar@schubergphilis.com>
2023-02-14 17:22:37 +0100

    Added new custom keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -22,3 +22,7 @@ keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
 keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
 keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 keymap("n", "<F26>", "<cmd>lua require'dap'.terminate()<cr>", opts) -- Ctrl-F2
+
+-- Custom keymaps
+keymap('n', '<C-d>', '<C-d>zz', opts)
+keymap('n', '<C-u>', '<C-u>zz', opts)
