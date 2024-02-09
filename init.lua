d67beacd4a9c1c29aa6d6aadd8e97839599c33c8
Alexander <sanderv32@gmail.com>
2024-02-09 15:25:33 +0100

    Refactored to lazy_vim

--- init.lua
+++ init.lua
@@ -1,7 +1,1 @@
-require "core.settings"
-require "core.keymaps"
-require "core.plugins"
-require "core.plugin_config"
-
--- Create Q as alias for 'qa!'
-vim.cmd [[command! Q qa!]]
+require("flc")
--- lua/core/plugin_config/telescope.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, builtin = pcall(require, "telescope.builtin")
-if not status_ok then
-    return
-end
-
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader><Space>', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
