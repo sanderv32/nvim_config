991713228577c4c42e1f4019365c4c337c7a9ad4
Alexander <sanderv32@gmail.com>
2024-02-13 20:31:41 +0100

    Add more harpoon keybindings

--- lua/flc/lazy/harpoon.lua
+++ lua/flc/lazy/harpoon.lua
@@ -15,5 +15,9 @@ return {
         vim.keymap.set('n', '<leader>2', function () harpoon:list():select(2) end)
         vim.keymap.set('n', '<leader>3', function () harpoon:list():select(3) end)
         vim.keymap.set('n', '<leader>4', function () harpoon:list():select(4) end)
+        vim.keymap.set('n', '<leader>5', function () harpoon:list():select(5) end)
+        vim.keymap.set('n', '<leader>6', function () harpoon:list():select(6) end)
+        vim.keymap.set('n', '<leader>7', function () harpoon:list():select(7) end)
+        vim.keymap.set('n', '<leader>8', function () harpoon:list():select(8) end)
     end
 }
